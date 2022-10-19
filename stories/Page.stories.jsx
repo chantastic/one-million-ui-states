403f514ce672aef9b358d8469002d0b3fb915bdc
Michael Chan <mijoch@gmail.com>
2022-10-18 23:35:47 -0700

    refacto page component to be mock-able

--- stories/Page.stories.jsx
+++ stories/Page.stories.jsx
@@ -1,4 +1,6 @@
+import * as React from "react";
 import { within, userEvent } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
 
 import { Page } from "./Page";
 
@@ -13,9 +15,34 @@ export default {
 export const LoggedOut = {};
 
 export const LoggedIn = {
+  render: renderWithMockedData,
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
     const loginButton = await canvas.getByRole("button", { name: /Log in/i });
     await userEvent.click(loginButton);
+
+    expect(canvas.getByText(/Jane Doe/));
   },
 };
+
+function renderWithMockedData(args) {
+  function MockUser({ children }) {
+    const userState = React.useState(null);
+
+    return typeof children === "function" ? children(userState) : children;
+  }
+
+  return (
+    <MockUser>
+      {([user, setUser]) => (
+        <Page
+          {...args}
+          onLogin={() => setUser({ name: "Jane Doe" })}
+          onLogout={() => setUser(null)}
+          onCreateAccount={() => setUser({ name: "Jane Doe" })}
+          user={user}
+        />
+      )}
+    </MockUser>
+  );
+}

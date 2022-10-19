fd344075359cc1ffd75f20f62cb4cfaa910df5d0
Michael Chan <mijoch@gmail.com>
2022-10-18 22:34:39 -0700

    convert story files to CSF 3 syntax (for demos)

--- stories/Header.stories.jsx
+++ stories/Header.stories.jsx
@@ -1,24 +1,19 @@
-import React from 'react';
-
-import { Header } from './Header';
+import { Header } from "./Header";
 
 export default {
-  title: 'Example/Header',
+  title: "Example/Header",
   component: Header,
   parameters: {
-    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
+    layout: "fullscreen",
   },
 };
 
-const Template = (args) => <Header {...args} />;
-
-export const LoggedIn = Template.bind({});
-LoggedIn.args = {
-  user: {
-    name: 'Jane Doe',
+export const LoggedIn = {
+  args: {
+    user: {
+      name: "Jane Doe",
+    },
   },
 };
 
-export const LoggedOut = Template.bind({});
-LoggedOut.args = {};
+export const LoggedOut = {};

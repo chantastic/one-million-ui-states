106bc5dbdb95180aa61a23df56ba7cad37b42aec
Michael Chan <mijoch@gmail.com>
2024-04-18 18:11:05 -0400

    Add lesson 4: set story viewport

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 [Lesson 1: Anatomy of a Story](./src/stories/Button.stories.js#L5)
 [Lesson 2: Compose stories](./src/stories/Button.stories.js#L60)
 [Lesson 3: Document extreme cases](./src/stories/Header.stories.js#L29)
+[Lesson 4: Set story viewport](./src/stories/Header.stories.js#L54)
+[Lesson 5: Anatomy of a play function](./src/stories/Page.stories.js#L16)
--- src/stories/Header.stories.js
+++ src/stories/Header.stories.js
@@ -49,3 +49,29 @@ Uncomment (or re-write) the code below to document both short and long extreme c
 //     },
 //   },
 // };
+
+/*
+Lesson 4: Set story viewport
+
+Using paramaters (in addition to args), we describe full UI cases.
+Like the combination of a long name in small viewport.
+
+Uncomment (or rewrite) the story below to see how long names render on mobile.
+Note that this requires args from the `LoggedInWithLongName` story.
+*/
+
+// export const LoggedInWithLongNameOnMobile = {
+//   args: LoggedInWithLongName.args,
+//   parameters: {
+//     viewport: {
+//       defaultViewport: "mobile1",
+//     },
+//   },
+// };
+
+/*
+NOTE:
+The viewport API is incredibly strange.
+"mobile1" is a magic string in Storybook.
+Read more about viewports here: https://storybook.js.org/docs/essentials/viewport#minimal_viewports
+*/

02b874989703701381571230505290bb419022ff
Michael Chan <mijoch@gmail.com>
2024-04-18 16:54:27 -0400

    remove chromatic ad's

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,8 +5,7 @@ const config = {
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    "@storybook/addon-interactions"
   ],
   framework: {
     name: "@storybook/react-vite",
